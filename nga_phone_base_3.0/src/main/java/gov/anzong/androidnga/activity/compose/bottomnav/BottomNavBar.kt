5dd8aacae3b111470b93ac2c676646cd63f2103c
trance <trance@example.com>
2026-06-30 13:22:33 +0800

    底栏图标大小

--- nga_phone_base_3.0/src/main/java/gov/anzong/androidnga/activity/compose/bottomnav/BottomNavBar.kt
+++ nga_phone_base_3.0/src/main/java/gov/anzong/androidnga/activity/compose/bottomnav/BottomNavBar.kt
@@ -1,16 +1,17 @@
 package gov.anzong.androidnga.activity.compose.bottomnav
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -54,13 +55,13 @@ fun BottomNavBar(
                 onClick = { onTabSelected(index) },
                 alwaysShowLabel = false,
                 icon = {
-                    Icon(
-                        painter = painterResource(id = item.iconResId),
-                        contentDescription = item.label,
-                        modifier = Modifier
-                            .fillMaxHeight(0.9f)
-                            .padding(2.dp),
-                    )
+                    Box(modifier = Modifier.size(BottomBarHeight), contentAlignment = Alignment.Center) {
+                        Icon(
+                            painter = painterResource(id = item.iconResId),
+                            contentDescription = item.label,
+                            modifier = Modifier.fillMaxSize(0.8f),
+                        )
+                    }
                 },
                 label = null,
                 colors = NavigationBarItemDefaults.colors(
